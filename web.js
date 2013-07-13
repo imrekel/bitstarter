8c6cdb3bb18daa36419ba97415e0bb23f0045bec
Imre Kelényi <imre.kelenyi@gmail.com>
2013-07-13 15:56:18 +0200

    Reading string from file

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
+var fs = require('fs');
+
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+
+  var stringFromFile = fs.readFileSync('index.html').toString();
+  response.send(stringFromFile);
+
 });
 
 var port = process.env.PORT || 5000;
